0820935f4a79706a25183c58194dfde5b1e1f8f9
Abdullah.Jaffer <abdullah@A002-00052.arbisoft.com>
2019-07-19 12:38:41 +0500

    shortened line length and changed file names

--- string_manipulation/main.py
+++ string_manipulation/main.py
@@ -1,4 +1,4 @@
-from helper import remove_first_occurrence_of_character
+from helper import remove_character_first_occurance
 from helper import repeating_to_single_string_converter
 
 
@@ -16,10 +16,12 @@ def main():
         output_file.write("Function 1:\n")
 
         for given_word in input_file_list:
-            output_file_output = repeating_to_single_string_converter(given_word)
+            output_file_output = \
+                repeating_to_single_string_converter(given_word)
             output_file.write(output_file_output)
             first_occurrence_removal_result = \
-                first_occurrence_removal_result + remove_first_occurrence_of_character(given_word)
+                first_occurrence_removal_result + \
+                remove_character_first_occurance(given_word)
 
         output_file.write("\nFunction 2:\n")
         output_file.write(first_occurrence_removal_result)
---
+++ string_manipulation/unit_tests.py
@@ -0,0 +1,54 @@
+import unittest
+
+from ddt import ddt, data, unpack
+
+from helper import is_repeating
+from helper import remove_character_first_occurance
+from helper import remove_given_character
+from helper import repeating_to_single_string_converter
+
+
+@ddt
+class TestStringMethods(unittest.TestCase):
+
+    @data(["TechCity", "Techiy"], ["TechTeam", "Techam"],
+          ["Umbrella", "Umbrela"], ["Data", "Dat"],
+          ["Google", "Gole"])
+    @unpack
+    def test_repeating_to_single_string_converter(self,
+                                                  input_value,
+                                                  output_value):
+        self.assertTrue(repeating_to_single_string_converter(input_value)
+                        ==
+                        output_value)
+
+    @data(["TechCity", "c"], ["TechCity", "C"],
+          ["Umbrella", "L"], ["Data", "a"])
+    @unpack
+    def test_is_repeating(self, input_value1,
+                          input_value2):
+        self.assertTrue(is_repeating(input_value1,
+                                     input_value2))
+
+    @data(["TechCity", "T", "echCity"],
+          ["TechTeam", "m", "TechTea"],
+          ["Umbrella", "e", "Umbrlla"])
+    @unpack
+    def test_remove_given_character(self, input_value1,
+                                    input_value2, output_value):
+        self.assertEqual(remove_given_character(input_value1,
+                                                input_value2),
+                         output_value)
+
+    @data(["TechCity", "ehCity"], ["TechTeam", "chTeam"],
+          ["Umbrella", "Umbrela"], ["Data", "Dta"],
+          ["Google", "ogle"])
+    @unpack
+    def test_remove_first_occurence_of_character(self, input_value,
+                                                 output_value):
+        self.assertEqual(remove_character_first_occurance(input_value),
+                         output_value)
+
+
+if __name__ == "__main__":
+    unittest.main()
