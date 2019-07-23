211aaaaf9e5d13bea813d818ce0b9edfb2c3630c
Abdullah.Jaffer <abdullah@A002-00052.arbisoft.com>
2019-07-23 15:08:00 +0500

    code break handled

--- TwitterMining/main.py
+++ TwitterMining/main.py
@@ -14,15 +14,18 @@ def main():
     parser.add_argument("-n", "--number", type=int,
                         help="number of tweets")
     args = parser.parse_args()
-    tweet_classifier = TweetClassifier(args.string, args.number, "tweets")
-    tweet_list = tweet_classifier.classify_tweets()
+    if args.string is not None and args.number is not None:
+        tweet_classifier = TweetClassifier(args.string, args.number, "tweets")
+        tweet_list = tweet_classifier.classify_tweets()
 
-    tweet_index = 0
-    while tweet_index < len(tweet_list):
-        print(tweet_list[tweet_index][0] + "  ||  classification: " +
-              tweet_list[tweet_index][1] + "  || tweet number: " +
-              str(tweet_index+1)+" ||")
-        tweet_index = tweet_index + 1
+        tweet_index = 0
+        while tweet_index < len(tweet_list):
+            print(tweet_list[tweet_index][0] + "  ||  classification: " +
+                  tweet_list[tweet_index][1] + "  || tweet number: " +
+                  str(tweet_index + 1) + " ||")
+            tweet_index = tweet_index + 1
+    else:
+        print("Incorrect or no input values given, please try again")
 
 
 if __name__ == "__main__":
