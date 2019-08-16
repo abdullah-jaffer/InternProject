1b35e020bcce29b104b786895858a66994344098
Abdullah.Jaffer <abdullah@A002-00052.arbisoft.com>
2019-08-16 12:51:09 +0500

    initial commit

---
+++ newsAPI/news/views.py
@@ -0,0 +1,34 @@
+from django.core import serializers
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+from news.forms import ArticleForm
+from news.models import Article
+
+
+def index(request):
+    return HttpResponse("Welcome, you're at the news app.")
+
+
+def get_news(request):
+    news_id = request.GET.get('id', '')
+    article = Article.objects.filter(id=news_id)
+    data = serializers.serialize('json', article)
+    return HttpResponse(data, content_type="application/json")
+
+
+def post_news(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = Article(title=request.POST.get('title'),
+                              pub_date=request.POST.get('pub_date'),
+                              category=request.POST.get('category'),
+                              cover_image=request.POST.get('cover_image'),
+                              content=request.POST.get('content'),
+                              author=request.POST.get('author'))
+            article.save()
+            return HttpResponseRedirect('/news/')
+    else:
+        form = ArticleForm()
+    return render(request, 'news/form.html', {'form': form})
